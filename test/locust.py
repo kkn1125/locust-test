e9ae7cf92818a0efbddf8c1a57d73d73a417b623
kkn1125 <xpfjrnseks1@naver.com>
2025-03-18 19:43:26 +0900

    ✨ feat: locust 부하 테스트

--- test/locust.py
+++ test/locust.py
@@ -1,2 +1,13 @@
 from locust import HttpUser, task
 
+baseUrl = "http://localhost:8080/api"
+
+
+class HelloWorldUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.post(
+            "/auth/login",
+            {"email": "chaplet01@gmail.com", "password": "qweQQ!!1"},
+        )
+        self.client.get("/users")
